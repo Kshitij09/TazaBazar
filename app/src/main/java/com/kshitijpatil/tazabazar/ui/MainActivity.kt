2198f75a6a29326ae8d8c77034d775d37c21563b
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-23 18:03:09 +0530

    Clear EditText focus when clicked outside
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/ui/MainActivity.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.kshitijpatil.tazabazar.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -16,5 +20,19 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         binding.bottomNavBar.setupWithNavController(navHostFragment.navController)
         setContentView(binding.root)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        clearFocusOnOutSideClick();
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun clearFocusOnOutSideClick() {
+        currentFocus?.apply {
+            if (this is EditText)
+                clearFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
     }
 }
