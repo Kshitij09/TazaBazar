d6dd5d6c0368e08a296ce5686db1cae12a1a970a
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-23 11:59:59 +0530

    Update comments, move log events, init timber
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/TazaBazarApplication.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/TazaBazarApplication.kt
@@ -2,10 +2,14 @@ package com.kshitijpatil.tazabazar
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
 
 class TazaBazarApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
