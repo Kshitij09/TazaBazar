5aa83c93ccd79e899e1064328d73d1916e03bc1a
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-27 23:43:13 +0530

    Add toggleable favorite button
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductListAdapter.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductListAdapter.kt
@@ -3,11 +3,13 @@ package com.kshitijpatil.tazabazar.ui.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.request.CachePolicy
+import com.google.android.material.button.MaterialButton
 import com.kshitijpatil.tazabazar.R
 import com.kshitijpatil.tazabazar.databinding.ProductItemViewBinding
 import com.kshitijpatil.tazabazar.model.Product
@@ -48,6 +50,7 @@ class ProductListAdapter :
         private val binding: ProductItemViewBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         private val contextRef = WeakReference(binding.root.context)
+        private var isFavorite = false
 
         fun bind(item: Product) {
             binding.tvName.text = item.name
@@ -58,7 +61,24 @@ class ProductListAdapter :
                 binding.tvPrice.text =
                     it.getString(R.string.info_price_rupee_template, inventory.price)
             }
+            updateFavoriteButtonColors(binding.btnFavorite, isFavorite)
+            binding.btnFavorite.setOnClickListener {
+                isFavorite = !isFavorite
+                updateFavoriteButtonColors(binding.btnFavorite, isFavorite)
+            }
             loadImage(binding.ivImage, item.imageUri)
+        }
+
+        private fun updateFavoriteButtonColors(btnFavorite: MaterialButton, favorite: Boolean) {
+            if (favorite) {
+                btnFavorite.setIconTintResource(R.color.tzb_green)
+                btnFavorite.backgroundTintList =
+                    ContextCompat.getColorStateList(btnFavorite.context, R.color.tzb_green_light)
+            } else {
+                btnFavorite.setIconTintResource(R.color.tzb_gray_200)
+                btnFavorite.backgroundTintList =
+                    ContextCompat.getColorStateList(btnFavorite.context, R.color.tzb_gray_100)
+            }
         }
 
         private fun loadImage(imageView: ImageView, imageUri: String) {
