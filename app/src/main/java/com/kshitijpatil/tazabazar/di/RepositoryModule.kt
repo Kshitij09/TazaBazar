f39d1343305175ab03a53a0e125024b033c0e03c
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-27 13:56:35 +0530

    Use lazy initializations
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/di/RepositoryModule.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/di/RepositoryModule.kt
@@ -1,7 +1,11 @@
 package com.kshitijpatil.tazabazar.di
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
+import androidx.room.Room
 import com.kshitijpatil.tazabazar.api.ApiModule
+import com.kshitijpatil.tazabazar.api.ProductApi
+import com.kshitijpatil.tazabazar.data.ProductDataSource
 import com.kshitijpatil.tazabazar.data.ProductRepository
 import com.kshitijpatil.tazabazar.data.ProductRepositoryImpl
 import com.kshitijpatil.tazabazar.data.local.AppDatabase
@@ -13,29 +17,60 @@ import okhttp3.OkHttpClient
 object RepositoryModule {
     private val appDispatchers = AppModule.provideAppCoroutineDispatchers()
 
+    private val lock = Any()
+    private var database: AppDatabase? = null
+
+    @Volatile
+    var productRepository: ProductRepository? = null
+        @VisibleForTesting set
+
+
     fun provideProductRepository(context: Context, okhttpClient: OkHttpClient): ProductRepository {
+        synchronized(lock) {
+            return productRepository ?: createProductRepository(context, okhttpClient)
+        }
+    }
+
+    fun createProductRepository(context: Context, okhttpClient: OkHttpClient): ProductRepository {
+        val appDatabase = database ?: createDatabase(context)
         val api = ApiModule.provideProductApi(okhttpClient)
-        val remoteDataSource = ProductRemoteDataSource(
-            productApi = api,
-            categoryMapper = MapperModule.productCategoryDtoToProductCategory,
-            productMapper = MapperModule.productResponseToProduct
-        )
-        val appDatabase = AppDatabase.buildDatabase(context)
-        val localDataSource = ProductLocalDataSource(
-            productDao = appDatabase.productDao,
-            productMapper = MapperModule.ProductWithInventoriesToProduct,
-            productCategoryDao = appDatabase.productCategoryDao
-        )
         val networkUtils = provideNetworkUtils(context)
-        return ProductRepositoryImpl(
-            remoteDataSource,
-            localDataSource,
+        val newRepo = ProductRepositoryImpl(
+            provideRemoteDataSource(api),
+            provideLocalDataSource(appDatabase),
             networkUtils,
             appDatabase,
             appDispatchers,
             MapperModule.productToProductWithInventories,
             MapperModule.productCategoryToProductCategoryEntity
         )
+        productRepository = newRepo
+        return newRepo
+    }
+
+    fun provideLocalDataSource(appDatabase: AppDatabase): ProductDataSource {
+        return ProductLocalDataSource(
+            productDao = appDatabase.productDao,
+            productMapper = MapperModule.ProductWithInventoriesToProduct,
+            productCategoryDao = appDatabase.productCategoryDao
+        )
+    }
+
+    fun provideRemoteDataSource(api: ProductApi): ProductDataSource {
+        return ProductRemoteDataSource(
+            productApi = api,
+            categoryMapper = MapperModule.productCategoryDtoToProductCategory,
+            productMapper = MapperModule.productResponseToProduct
+        )
+    }
+
+    fun createDatabase(context: Context): AppDatabase {
+        val result =
+            Room.databaseBuilder(context, AppDatabase::class.java, AppDatabase.databaseName)
+                .fallbackToDestructiveMigration()
+                .build()
+        database = result
+        return result
     }
 
     fun provideNetworkUtils(context: Context): NetworkUtils = NetworkUtils(context)
