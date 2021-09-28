791a46ebd9a68a8cc53d9abb3ae67c35ebe576ef
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-28 10:31:47 +0530

    Move ProductViewHolder out of ListAdapter
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductListAdapter.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductListAdapter.kt
@@ -2,21 +2,12 @@ package com.kshitijpatil.tazabazar.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import coil.load
-import coil.request.CachePolicy
-import com.google.android.material.button.MaterialButton
-import com.kshitijpatil.tazabazar.R
 import com.kshitijpatil.tazabazar.databinding.ProductItemViewBinding
 import com.kshitijpatil.tazabazar.model.Product
-import java.lang.ref.WeakReference
 
-class ProductListAdapter :
-    ListAdapter<Product, ProductListAdapter.ProductViewHolder>(ProductDiffCallback()) {
+class ProductListAdapter : ListAdapter<Product, ProductViewHolder>(ProductDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -41,54 +32,5 @@ class ProductListAdapter :
             return oldItem == newItem
         }
 
-    }
-
-    // TODO: Handle out of stock
-    // TODO: Handle discount details
-    // TODO: Handle Cart Action
-    class ProductViewHolder(
-        private val binding: ProductItemViewBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-        private val contextRef = WeakReference(binding.root.context)
-        private var isFavorite = false
-
-        fun bind(item: Product) {
-            binding.tvName.text = item.name
-            // TODO: Make it list menu
-            val inventory = item.inventories[0]
-            binding.tvQuantityLabel.text = inventory.quantityLabel
-            contextRef.get()?.let {
-                binding.tvPrice.text =
-                    it.getString(R.string.info_price_rupee_template, inventory.price)
-            }
-            updateFavoriteButtonColors(binding.btnFavorite, isFavorite)
-            binding.btnFavorite.setOnClickListener {
-                isFavorite = !isFavorite
-                updateFavoriteButtonColors(binding.btnFavorite, isFavorite)
-            }
-            loadImage(binding.ivImage, item.imageUri)
-        }
-
-        private fun updateFavoriteButtonColors(btnFavorite: MaterialButton, favorite: Boolean) {
-            if (favorite) {
-                btnFavorite.setIconTintResource(R.color.tzb_green)
-                btnFavorite.backgroundTintList =
-                    ContextCompat.getColorStateList(btnFavorite.context, R.color.tzb_green_light)
-            } else {
-                btnFavorite.setIconTintResource(R.color.tzb_gray_200)
-                btnFavorite.backgroundTintList =
-                    ContextCompat.getColorStateList(btnFavorite.context, R.color.tzb_gray_100)
-            }
-        }
-
-        private fun loadImage(imageView: ImageView, imageUri: String) {
-            imageView.load(imageUri) {
-                crossfade(true)
-                placeholder(R.drawable.product_preview_placeholder)
-                error(R.drawable.product_preview_placeholder)
-                memoryCachePolicy(CachePolicy.ENABLED)
-                memoryCacheKey(imageUri)
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductViewHolder.kt
@@ -0,0 +1,60 @@
+package com.kshitijpatil.tazabazar.ui.home
+
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import com.google.android.material.button.MaterialButton
+import com.kshitijpatil.tazabazar.R
+import com.kshitijpatil.tazabazar.databinding.ProductItemViewBinding
+import com.kshitijpatil.tazabazar.model.Product
+
+// TODO: Handle out of stock
+// TODO: Handle discount details
+// TODO: Handle Cart Action
+class ProductViewHolder(
+    private val binding: ProductItemViewBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    private var isFavorite = false
+
+    fun bind(item: Product) {
+        binding.tvName.text = item.name
+        // TODO: Make it list menu
+        val inventory = item.inventories[0]
+        binding.tvQuantityLabel.text = inventory.quantityLabel
+        binding.tvPrice.text = getPriceStringFor(inventory.price)
+        updateFavoriteButtonColors(binding.btnFavorite, isFavorite)
+        binding.btnFavorite.setOnClickListener {
+            isFavorite = !isFavorite
+            updateFavoriteButtonColors(binding.btnFavorite, isFavorite)
+        }
+        loadImage(binding.ivImage, item.imageUri)
+    }
+
+    private fun getPriceStringFor(price: Float): String {
+        return binding.root.context.getString(R.string.info_price_rupee_template, price)
+    }
+
+    private fun updateFavoriteButtonColors(btnFavorite: MaterialButton, favorite: Boolean) {
+        if (favorite) {
+            btnFavorite.setIconTintResource(R.color.tzb_green)
+            btnFavorite.backgroundTintList =
+                ContextCompat.getColorStateList(btnFavorite.context, R.color.tzb_green_light)
+        } else {
+            btnFavorite.setIconTintResource(R.color.tzb_gray_200)
+            btnFavorite.backgroundTintList =
+                ContextCompat.getColorStateList(btnFavorite.context, R.color.tzb_gray_100)
+        }
+    }
+
+    private fun loadImage(imageView: ImageView, imageUri: String) {
+        imageView.load(imageUri) {
+            crossfade(true)
+            placeholder(R.drawable.product_preview_placeholder)
+            error(R.drawable.product_preview_placeholder)
+            memoryCachePolicy(CachePolicy.ENABLED)
+            memoryCacheKey(imageUri)
+        }
+    }
+}
