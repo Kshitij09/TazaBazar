921a9d26d096047cba72f8ac4b0e4a570ed4bcf3
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-30 19:24:43 +0530

    Implement FavoriteFragment UI
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/favorite/FavoriteFragment.kt
@@ -1,7 +1,28 @@
 package com.kshitijpatil.tazabazar.ui.favorite
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kshitijpatil.tazabazar.R
+import com.kshitijpatil.tazabazar.databinding.FragmentFavoriteBinding
 
-class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding: FragmentFavoriteBinding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        binding.cardviewWeekly.setOnClickListener { }
+        binding.cardviewMonthly.setOnClickListener { }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
