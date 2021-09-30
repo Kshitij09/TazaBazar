489ab721f492f78133041fc4937e1a0b4246f204
Kshitij Patil <kshitijpatil98@gmail.com>
2021-10-01 01:06:46 +0530

    Make ProductListAdapter & ViewHolder generic
    Supports both grid and row layouts
    
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductListAdapter.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductListAdapter.kt
@@ -4,17 +4,21 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.kshitijpatil.tazabazar.databinding.ProductItemViewBinding
+import com.kshitijpatil.tazabazar.R
 import com.kshitijpatil.tazabazar.model.Product
 
 class ProductListAdapter(
+    private val layoutType: ProductLayoutType = ProductLayoutType.GRID,
     var onItemActionCallback: ProductViewHolder.OnItemActionCallback? = null
 ) : ListAdapter<Product, ProductViewHolder>(ProductDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ProductItemViewBinding.inflate(layoutInflater, parent, false)
-        return ProductViewHolder(binding, onItemActionCallback)
+        val layoutResource = when (layoutType) {
+            ProductLayoutType.GRID -> R.layout.product_grid_item_view
+            ProductLayoutType.ROW -> R.layout.product_row_item_view
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layoutResource, parent, false)
+        return ProductViewHolder(view, onItemActionCallback)
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
@@ -34,5 +38,9 @@ class ProductListAdapter(
             return oldItem == newItem
         }
 
+    }
+
+    enum class ProductLayoutType {
+        GRID, ROW
     }
 }
--- app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductViewHolder.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/home/ProductViewHolder.kt
@@ -1,45 +1,51 @@
 package com.kshitijpatil.tazabazar.ui.home
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.request.CachePolicy
 import com.google.android.material.button.MaterialButton
 import com.kshitijpatil.tazabazar.R
-import com.kshitijpatil.tazabazar.databinding.ProductItemViewBinding
 import com.kshitijpatil.tazabazar.model.Product
+import java.lang.ref.WeakReference
 
 // TODO: Handle out of stock
 // TODO: Handle discount details
 // TODO: Handle Cart Action
 class ProductViewHolder(
-    private val binding: ProductItemViewBinding,
+    view: View,
     var onItemActionCallback: OnItemActionCallback? = null
-) : RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(view) {
+    private val viewRef = WeakReference(view)
+    private val tvName: TextView = view.findViewById(R.id.tv_name)
+    private val tvPrice: TextView = view.findViewById(R.id.tv_price)
+    private val tvQuantityLabel: TextView = view.findViewById(R.id.tv_quantity_label)
+    private val btnFavorite: MaterialButton = view.findViewById(R.id.btn_favorite)
+    private val ivImage: ImageView = view.findViewById(R.id.iv_image)
     fun bind(item: Product) {
-        binding.tvName.text = item.name
+        tvName.text = item.name
         // TODO: Make it list menu
         val inventory = item.inventories[0]
-        binding.tvQuantityLabel.text = inventory.quantityLabel
-        binding.tvPrice.text = getPriceStringFor(inventory.price)
-        updateFavoriteButtonColors(binding.btnFavorite, item.favorites.isNotEmpty())
-        binding.btnFavorite.setOnClickListener {
+        tvQuantityLabel.text = inventory.quantityLabel
+        viewRef.get()?.context?.let {
+            tvPrice.text = it.getString(R.string.info_price_rupee_template, inventory.price)
+        }
+        updateFavoriteButtonColors(item.favorites.isNotEmpty())
+        btnFavorite.setOnClickListener {
             // Update favorite colors in advance
             // since item will be added in weekly list
             // by default
             if (item.favorites.isEmpty())
-                updateFavoriteButtonColors(binding.btnFavorite, true)
+                updateFavoriteButtonColors(true)
             onItemActionCallback?.onFavoriteClicked(item)
         }
-        loadImage(binding.ivImage, item.imageUri)
+        loadImage(item.imageUri)
     }
 
-    private fun getPriceStringFor(price: Float): String {
-        return binding.root.context.getString(R.string.info_price_rupee_template, price)
-    }
-
-    private fun updateFavoriteButtonColors(btnFavorite: MaterialButton, favorite: Boolean) {
+    private fun updateFavoriteButtonColors(favorite: Boolean) {
         if (favorite) {
             btnFavorite.setIconTintResource(R.color.tzb_green)
             btnFavorite.backgroundTintList =
@@ -51,8 +57,8 @@ class ProductViewHolder(
         }
     }
 
-    private fun loadImage(imageView: ImageView, imageUri: String) {
-        imageView.load(imageUri) {
+    private fun loadImage(imageUri: String) {
+        ivImage.load(imageUri) {
             placeholder(R.drawable.product_preview_placeholder)
             error(R.drawable.product_preview_placeholder)
             memoryCachePolicy(CachePolicy.ENABLED)
