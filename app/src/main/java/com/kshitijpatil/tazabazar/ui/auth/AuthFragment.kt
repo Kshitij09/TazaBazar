ee49577958ec6486af420795acf730babf8e0942
Kshitij Patil <kshitijpatil98@gmail.com>
2021-10-05 17:25:58 +0530

    Initiate Auth Navigation Flow
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

---
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/auth/AuthFragment.kt
@@ -0,0 +1,6 @@
+package com.kshitijpatil.tazabazar.ui.auth
+
+import androidx.fragment.app.Fragment
+import com.kshitijpatil.tazabazar.R
+
+class AuthFragment : Fragment(R.layout.fragment_auth)
--- app/src/main/java/com/kshitijpatil/tazabazar/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/kshitijpatil/tazabazar/ui/profile/ProfileFragment.kt
@@ -1,7 +1,23 @@
 package com.kshitijpatil.tazabazar.ui.profile
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.kshitijpatil.tazabazar.R
+import com.kshitijpatil.tazabazar.ui.DashboardFragmentDirections
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnLogin = view.findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener { openAuthNavigation() }
+    }
+
+    private fun openAuthNavigation() {
+        val direction = DashboardFragmentDirections.actionFragmentDashboardToNavigationAuth()
+        requireActivity().findNavController(R.id.main_activity_nav_host_fragment)
+            .navigate(direction)
+    }
 }
