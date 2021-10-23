9b4ce383725d93701f44c72f8e83fcfec95e9ed0
Kshitij Patil <kshitijpatil98@gmail.com>
2021-10-23 13:21:21 +0530

    Add `Order` domain model and mapper for the same
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

---
+++ app/src/main/java/com/kshitijpatil/tazabazar/model/Order.kt
@@ -0,0 +1,19 @@
+package com.kshitijpatil.tazabazar.model
+
+import org.threeten.bp.LocalDateTime
+
+data class OrderLine(
+    val inventoryId: Int,
+    val quantity: Int
+)
+
+enum class OrderStatus {
+    ACCEPTED, PENDING, DISPATCHED, DELIVERED, CANCELLED
+}
+
+data class Order(
+    val createdAt: LocalDateTime,
+    val orderId: String,
+    val orderLines: List<OrderLine>,
+    val status: OrderStatus,
+)
